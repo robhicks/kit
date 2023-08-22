bd0d56a4657527c677d0dae795c7f01eef9c6d72
Rob Hicks <robert.hicks@familysearch.org>
2023-08-22 15:14:50 -0600

    try to add clustering to adapter-node

--- packages/adapter-node/src/index.js
+++ packages/adapter-node/src/index.js
@@ -1,15 +1,50 @@
-import { handler } from 'HANDLER';
-import { env } from 'ENV';
-import polka from 'polka';
+import { handler } from 'HANDLER'
+import { env } from 'ENV'
+import polka from 'polka'
+import throng from 'throng'
 
-export const path = env('SOCKET_PATH', false);
-export const host = env('HOST', '0.0.0.0');
-export const port = env('PORT', !path && '3000');
+export const path = env('SOCKET_PATH', false)
+export const host = env('HOST', '0.0.0.0')
+export const port = env('PORT', !path && '3000')
 
-const server = polka().use(handler);
+const server = polka().use(handler)
 
-server.listen({ path, host, port }, () => {
-	console.log(`Listening on ${path ? path : host + ':' + port}`);
-});
+function master() {
+  console.log('Started master')
 
-export { server };
+  process.on('beforeExit', () => {
+    console.log('Master cleanup.')
+  })
+}
+
+/**
+ * @param {any} id
+ * @param {() => void} disconnect
+ */
+function worker(id, disconnect) {
+  let exited = false
+  server.listen({ path, host, port }, () => {
+    console.log(`Listening on ${path ? path : `${host}:${port}`}`)
+  })
+  console.log(`Started worker ${id}`)
+  process.on('SIGTERM', shutdown)
+  process.on('SIGINT', shutdown)
+
+  async function shutdown() {
+    if (exited) return
+    exited = true
+
+    await new Promise((r) => setTimeout(r, 300)) // simulate async cleanup work
+    console.log(`Worker ${id} cleanup done.`)
+    disconnect()
+  }
+}
+
+throng({
+  workers: env('WEB_CONCURRENCY', 1),
+  lifetime: Infinity,
+  master,
+  worker,
+})
+
+export { server }
